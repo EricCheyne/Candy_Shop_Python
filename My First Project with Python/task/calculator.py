278feaa026af515c461f0871a67df6f0d9aa17fe
ericcheyne <ericcheyne@gmail.com>
2025-03-11 00:47:10 -0700

    Calculator

---
+++ My First Project with Python/task/calculator.py
@@ -0,0 +1,12 @@
+# Write your code here
+total_earned = 202 + 118 + 2250 + 1680 + 1075 + 80
+
+print('''Earned amount:
+Bubblegum: $202
+Toffee: $118
+Ice cream: $2250
+Milk chocolate: $1680
+Doughnut: $1075
+Pancake: $80
+
+Income: $''' + str(total_earned))
